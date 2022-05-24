54685b6e0af09d58ef6f02bf6191f29730852a4f
Luciana Zanetti <luciana.zeta@gmail.com>
2022-05-24 13:42:25 -0300

    todoList -Todo

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+const Todo = () => {
+    return (
+        <div className="todo">
+            <li className="todo-item">listooooo</li>
+            <button className="complet-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button  className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,13 @@
+import Todo from './Todo';
 
 const  TodoList = () => {
     return(
     <div className="todo-container">
-        <ul className="todo-list"></ul>
+        <ul className="todo-list">
+            <Todo />
+        </ul>
     </div>
-    )
+    );
 }
 
 export default TodoList;
