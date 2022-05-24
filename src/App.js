e5b517fa6f01a844b22315bf1475586821b8fe10
Luciana Zanetti <luciana.zeta@gmail.com>
2022-05-24 12:09:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
+//Importing Components
+import Form from './components/Form'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React todo List</h1>
+      <header>
+      <h1>Todo List</h1>
+      </header>
+      <Form />
     </div>
+
   );
 }
 
